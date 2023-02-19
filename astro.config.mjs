51321c25e5cbcc9d503ad9968772ea2efb83e2df
wuxue <wuxue@windimg.com>
2023-02-19 19:07:41 +0800

    eslint

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,9 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
+import eslintPlugin from 'eslint-plugin-astro'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), eslintPlugin()]
 });
